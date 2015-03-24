1263b1f6eedbb81ff22977451862e138b111d2cc
Shaibu Junior <junioorshaibu@yahoo.com>
2015-03-24 01:00:12 +0100

    the function 'eval2' evaluates only operations involving the multiplication operator(*) and the division operator(/).

--- icalc.py
+++ icalc.py
@@ -32,4 +32,51 @@ def eval_(expr):
     # sum up all values in expr_results
     return sum(expr_results)
         
+        
+        
+        
+def eval2(expr):
+    ''''evaluates expressions containing only division and multiplication'''
+    
+    
+    expr_parts= expr and expr.split('*')
+    
+    expr_results=[]
+    
+    for expr in expr_parts:
+    #further splitting the '/' expressions
+        
+        more_parts= [x for x in expr.split('/') if x !='']
+        if len(more_parts)==1:
+        # to ensure that,the expression is valid(no characters)
+            try: 
+                int(expr)
+                expr_results.append(int(expr))
+            except:
+                raise ValueError('Invalid expression provided: %s' % expr)
+        else:
+            try:
+                parts=[int(x) for x in more_parts]
+                parts[0]= str(str(parts[0])+'.0')
+                results= float(parts[0])
+                    
+                for part in parts[1:]:
+                    results /= part
+                expr_results.append(results)
+            except:
+                raise ValueError('Invalid expression provided: %s' % expr)
+            
+            
+        x= expr_results[0]
+        for i in expr_results[1:]:
+            x*=i
+        
+    
+    if len(expr_results)<2 and type(expr_results[0])==str:
+        raise ValueError('Invalid expression provided:%s' %expr)
+        
+    
+    return x
+
+
     
